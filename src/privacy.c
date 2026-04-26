62a7b6aaa993dfe1b50321c027139dafc5867787
jana <4jana5@gmail.com>
2026-04-26 17:57:07 +0200

    Switch openat to open because cat calls open and not openat (see ltrace vs strace)

--- src/privacy.c
+++ src/privacy.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int open(const char *path, int flag, ...) {
-    fprintf(stdout, "hijacked functions for openat got called!");
+    printf("hijacked functions for open got called!\n");
     int(*real_open)(const char *, int, ...) = dlsym(RTLD_NEXT, "open");
 
     return real_open(path, flag);
