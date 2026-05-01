4ba57c861e4e9e4d8e22182f73b7c079217df635
Julia Sauer <julia.sauer@hotmail.ch>
2026-05-01 13:23:00 +0200

    add every hijacked function to same file

--- src/privacy.c
+++ src/privacy.c
@@ -2,10 +2,20 @@
 #include <fcntl.h>
 #include <dlfcn.h>
 #include <stdio.h>
+#include <errno.h>
 
 int open(const char *path, int flag, ...) {
     printf("hijacked functions for open got called!\n");
     int(*real_open)(const char *, int, ...) = dlsym(RTLD_NEXT, "open");
 
     return real_open(path, flag);
+}
+
+// remove hijack (rm in terminal)
+int unlinkat(int dirfd, const char *pathname, int flags) {
+    printf("Hijacked unlinkat() called!\n");
+    printf("You do not have permission to delete: %s\n", pathname);
+    
+    errno = EACCES;   // "Permission denied"
+    return -1;        // signals failure to the caller
 }
